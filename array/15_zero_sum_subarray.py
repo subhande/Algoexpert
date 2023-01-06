e507cdf1807cd88f87485855aaf9b3969da27b25
Subhan De <desubhan123@gmail.com>
2023-01-06 08:31:06 +0530

    6th jan commit

--- array/15_zero_sum_subarray.py
+++ array/15_zero_sum_subarray.py
@@ -1,11 +1,14 @@
 #  Zero Sum Subarray
 
 """
-You're given a list of integers nums. Write a function that returns true if there exists a subarray whose sum is equal to 0, and false otherwise.
+You're given a list of integers nums. Write a function that returns a boolean representing whether there exists a zero-sum subarray of nums.
 
-Constraints
+A zero-sum subarray is any subarray where all of the values add up to zero. A subarray is any contiguous section of the array. For the purposes of this problem, a subarray can be as small as one element and as long as the original array.
 
-n ≤ 100,000 where n is the length of nums
+Sample Input
+nums = [-5, -5, 2, 3, -2]
+Sample Output
+True // The subarray [-5, 2, 3] has a sum of 0
 
 """
 # Time: O(n) | Space: O(n)
