ba328005124b354284a0bba65deb404bfa3260d5
Subhan De <desubhan123@gmail.com>
2023-05-02 07:48:35 +0530

    2nd may DP-1

---
+++ dynamic_programming/01_max_subset_sum_no_adjacent.py
@@ -0,0 +1,54 @@
+# Max Subset Sum No Adjacent
+
+"""
+Write a function that takes in an array of positive integers and returns the maximum sum of non-adjacent elements in the array.
+
+If the input array is empty, the function should return 0.
+
+Sample Input
+array = [75, 105, 120, 75, 90, 135]
+
+Sample Output
+330 // 75 + 120 + 135
+
+
+Example 1:
+arr = [7, 10, 12, 7, 9, 14]
+
+[7, max(7, 10), max(7+12, 10), max(7+12, 10+7, 7+7), 7+12+9, 7+12+9+14]
+[7, 10, 19, 19, 28, 33]
+
+DP Formula:
+
+arr[i] = max(arr[i-1], arr[i-2] + arr[i])
+
+"""
+
+# Solution 1
+# Time: O(n) | Space: O(n)
+def maxSubsetSumNoAdjacent(array):
+    maxSums = array[:]
+    if len(array) == 0:
+        return 0
+    if len(array) == 1:
+        return array[0]
+    maxSums[1] = max(array[0], array[1])
+    for i in range(2, len(array)):
+            maxSums[i] = max(maxSums[i-1], maxSums[i-2] + array[i])
+    return maxSums[-1]
+
+
+# Solution 2
+# Time: O(n) | Space: O(1)
+def maxSubsetSumNoAdjacent(array):
+    if len(array) == 0:
+        return 0
+    if len(array) == 1:
+        return array[0]
+    second = array[0]
+    first = max(array[0], array[1])
+    for i in range(2, len(array)):
+        current = max(first, second + array[i])
+        second = first
+        first = current
+    return first
---
+++ dynamic_programming/02_number_of_ways_to_make_change.py
@@ -0,0 +1,45 @@
+# Number Of Ways To Make Change
+
+"""
+Given an array of positive integers representing coin denominations and a single non-negative integer n representing a target amount of money, write a function that returns the number of ways to make change for that target amount using the given coin denominations.
+
+Note that an unlimited amount of coins is at your disposal.
+
+
+Sample Input
+n = 6
+denoms = [1, 5]
+
+Sample Output
+2 // 1x1 + 1x5 and 6x1
+
+Example 1:
+$10 [1, 5, 10, 25]
+
+ways --> [1, 1, 1, 1, 1, 2, 2, 2, 2, 2,  4]
+         [-, -, -, -, -, -, -, -, -, -, --]
+         [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+
+        DP Formula:
+        ways[i] = ways[i] + ways[i - denom]
+
+
+"""
+
+# Solution 1
+# Time: O(n*d) | Space: O(n)
+def numberOfWaysToMakeChange(n, denoms):
+    ways = [0 for _ in range(n+1)]
+    ways[0] = 1
+
+    for denom in denoms:
+        for amount in range(1, n+1):
+            if denom <= amount:
+                ways[amount] = ways[amount] + ways[amount-denom]
+    return ways[n]
+
+if __name__ == "__main__":
+    n = 10
+    denoms = [1, 5, 10, 25]
+    print(numberOfWaysToMakeChange(n, denoms))
