fdfda00e8f96472b4f9dedc38966b454f3d83d96
Subhan De <desubhan@Subhans-MacBook-Air.local>
2023-10-20 09:26:52 +0530

    Revision Oct 20

--- previous/dynamic_programming/07_longest_common_subsequesnce.py
+++ previous/dynamic_programming/07_longest_common_subsequesnce.py
@@ -32,33 +32,38 @@ Explanation:
 
 # Solution 1
 
+
 # Time: O(nm*min(n, m)) | Space: O(nm*min(n, m))
 def longestCommonSubsequence(str1, str2):
     lcs = [[[] for x in range(len(str1) + 1)] for y in range(len(str2) + 1)]
     for i in range(1, len(str2) + 1):
         for j in range(1, len(str1) + 1):
-            if str2[i-1] == str1[j-1]:
-                lcs[i][j] = lcs[i-1][j-1] + [str2[i-1]]
+            if str2[i - 1] == str1[j - 1]:
+                lcs[i][j] = lcs[i - 1][j - 1] + [str2[i - 1]]
             else:
-                lcs[i][j] = max(lcs[i-1][j], lcs[i][j-1], key=len)
+                lcs[i][j] = max(lcs[i - 1][j], lcs[i][j - 1], key=len)
 
     return lcs[-1][-1]
 
+
 # Solution 3
+
 # Time: O(nm) | Space: O(nm)
 def longestCommonSubsequence(str1, str2):
     lcs = [[[None, 0, None, None] for x in range(len(str1) + 1)] for y in range(len(str2) + 1)]
+    # [None (current letter), 0 (length of current lcs), None (previous i), None (previous j)]
     for i in range(1, len(str2) + 1):
         for j in range(1, len(str1) + 1):
-            if str2[i-1] == str1[j-1]:
-                lcs[i][j] = [str2[i-1], str[i-1], str2[i-1][j-1], i-1, j-1]
+            if str2[i - 1] == str1[j - 1]:
+                lcs[i][j] = [str2[i - 1], str[i - 1], str2[i - 1][j - 1], i - 1, j - 1]
             else:
-                if len(lcs[i-1][j]) > len(lcs[i][j-1]):
-                    lcs[i][j] = [None, lcs[i-1][j], i-1, j]
+                if len(lcs[i - 1][j]) > len(lcs[i][j - 1]):
+                    lcs[i][j] = [None, lcs[i - 1][j], i - 1, j]
                 else:
-                    lcs[i][j] = [None, lcs[i][j-1], i, j-1]
+                    lcs[i][j] = [None, lcs[i][j - 1], i, j - 1]
 
     return buildSequence(lcs)
+
 
 def buildSequence(lcs):
     sequence = []
@@ -74,6 +79,7 @@ def buildSequence(lcs):
 
 
 # Solution 2
+
 
 # Time: O(nm*min(n, m)) | Space: O(min(n, m)^2)
 def longestCommonSubsequence(str1, str2):
@@ -91,10 +97,10 @@ def longestCommonSubsequence(str1, str2):
             currentLcs = evenLcs
             previousLcs = oddLcs
         for j in range(1, len(small) + 1):
-            if big[i-1] == small[j-1]:
-                currentLcs[j] = previousLcs[j-1] + [big[i-1]]
+            if big[i - 1] == small[j - 1]:
+                currentLcs[j] = previousLcs[j - 1] + [big[i - 1]]
             else:
-                currentLcs[j] = max(previousLcs[j], currentLcs[j-1], key=len)
+                currentLcs[j] = max(previousLcs[j], currentLcs[j - 1], key=len)
     return evenLcs[-1] if len(big) % 2 == 0 else oddLcs[-1]
 
 
@@ -104,24 +110,25 @@ def longestCommonSubsequence(str1, str2):
     lengths = [[0 for x in range(len(str1) + 1)] for y in range(len(str2) + 1)]
     for i in range(1, len(str2) + 1):
         for j in range(1, len(str1) + 1):
-            if str2[i-1] == str1[j-1]:
-                lengths[i][j] = lengths[i-1][j-1] + 1
+            if str2[i - 1] == str1[j - 1]:
+                lengths[i][j] = lengths[i - 1][j - 1] + 1
             else:
-                lengths[i][j] = max(lengths[i-1][j], lengths[i][j-1])
+                lengths[i][j] = max(lengths[i - 1][j], lengths[i][j - 1])
 
     return buildSequence(lengths, str1)
+
 
 def buildSequence(lengths, string):
     sequence = []
     i = len(lengths) - 1
     j = len(lengths[0]) - 1
     while i != 0 and j != 0:
-        if lengths[i][j] == lengths[i-1][j]:
+        if lengths[i][j] == lengths[i - 1][j]:
             i -= 1
-        elif lengths[i][j] == lengths[i][j-1]:
+        elif lengths[i][j] == lengths[i][j - 1]:
             j -= 1
         else:
-            sequence.append(string[j-1])
+            sequence.append(string[j - 1])
             i -= 1
             j -= 1
     return list(reversed(sequence))
--- previous/dynamic_programming/08_min_number_of_jumps.py
+++ previous/dynamic_programming/08_min_number_of_jumps.py
@@ -15,10 +15,23 @@ array = [3, 4, 2, 1, 2, 3, 7, 1, 1, 1, 3]
 Sample Output
 4 // 3 --> (4 or 2) --> (2 or 3) --> 7 --> 3
 
+Explaination:
+array = [3, 4, 2, 1, 2, 3, 7, 1, 1, 1, 3]
+jumps = [0, inf, inf, inf, inf, inf, inf, inf, inf, inf, inf, inf]
+
+
 DP Formula:
  jumps[i] = min(jumps[i], jumps[j] + 1) for all j < i and array[j] + j >= i
+ 
+or 
+
+where 0 <= j < i
+
+if array[j] + j >= i:
+jumps[i] = min(jumps[i], jumps[j]+1)
 
 """
+
 
 # Solution 1: Polynomial time complexity
 # Time: O(n^2) | Space: O(n)
@@ -30,11 +43,13 @@ def minNumberOfJumps(array):
         for j in range(0, i):
             if array[j] + j >= i:
                 jumps[i] = min(jumps[i], jumps[j] + 1)
-    
+
     return jumps[-1]
+
 
 # Solution 2: Linear time complexity
 # Time: O(n) | Space: O(1)
+
 
 def minNumberOfJumps(array):
     if len(array) == 1:
@@ -42,11 +57,10 @@ def minNumberOfJumps(array):
     jumps = 0
     max_reach = array[0]
     steps = array[0]
-    for i in range(1, len(array)-1):
+    for i in range(1, len(array) - 1):
         max_reach = max(max_reach, i + array[i])
         steps -= 1
         if steps == 0:
             jumps += 1
             steps = max_reach - i
     return jumps + 1
-
